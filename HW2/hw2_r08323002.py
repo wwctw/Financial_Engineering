130f770e7d823bc09514f755dbbc2ddac1f9ffe7
wwctw <53468345+wwctw@users.noreply.github.com>
2020-03-30 22:19:27 +0800

    Add files via upload

--- HW2/hw2_r08323002.py
+++ HW2/hw2_r08323002.py
@@ -4,64 +4,75 @@ Created on Sun Mar 29 02:30:27 2020
 
 @author: user01
 """
-
+# 債券殖利率計算函數ytm
+#   參數:  price: 現在價格  par: 票面價格  rate: 票面利率
+#          t_period: 總期數  payment: 每年計息次數
 def ytm(price,par,rate,t_period,payment):
     upper = 1.0
     C = par*rate/payment
+    # 每次增加100%去找殖利率的整數初始上界
     while 1:
         pay_inter = upper/payment
         PV = C*(1-(1+pay_inter)**(-t_period))/pay_inter \
              + par*(1+pay_inter)**(-t_period)
-        if PV > price:
+        if PV > price:     # 該利率得出的現值比價格高代表殖利率比該利率高
             upper = upper + 1.0
-        else:
+        else:              # 該利率得出的現值比價格低代表殖利率比該利率低
              break
-    lower = upper - 1.0
+    # 利用二分法來找殖利率
+    lower = upper - 1.0    # 初始下界
     mid = (lower+upper)/2
     for k in range(1,60):
         pay_inter = mid/payment
         PV = C*(1-(1+pay_inter)**(-t_period))/pay_inter \
              + par*(1+pay_inter)**(-t_period)
-        if PV > price:
+        if PV > price:     # 該利率得出的現值比價格高代表殖利率比該利率高
             lower = mid
-        else:
+        else:              # 該利率得出的現值比價格低代表殖利率比該利率低
             upper = mid
         mid = (lower+upper)/2
     return (lower+upper)/2
 
+# 輸入每年計息次數與年數
 payment = int(input(\
 "   請輸入每年計息次數   年計息請輸入 1   半年計息請輸入 2   季計息請輸入 4 :  "))
 end_year = float(input("   請輸入總年數 :  "))
 total_period = int(end_year*payment)
 print('\n   總共有 ',total_period,' 期')
 
-coupon_rate = [0]
-bond_price = [1000]
+coupon_rate = [0]    # 票面利率 List
+bond_price = [1000]  # 債券價格 List
 
-ytm_bond = [0]
-spot_rate = [0]
-forward_rate = [[]]
+ytm_bond = [0]       # 債券殖利率 List
+spot_rate = [0]      # 即期利率 List
+forward_rate = [[]]  # 遠期利率 二維List
 
-
-
+# 輸入債券票面利率與價格
 for k in range(1,total_period+1):
     print("\n   面額1000元到期時間",'%.2f'%(k/payment),"年債券的債券")
     coupon_rate.append(float(input("     請輸入票面利率(%) :  "))/100)
     bond_price.append(float(input("     請輸入債券價格 :  ")))
-    
+
+# 計算即期利率與遠期利率
 for k in range(1,total_period+1):
     ytm_bond.append( ytm(bond_price[k],1000,coupon_rate[k],k,payment) )
     temp_price = bond_price[k]
-    C = 1000*coupon_rate[k]/payment
+    C = 1000*coupon_rate[k]/payment # 每一期的利息現金流
+    # 計算債券最後一期現金流的現值
     for i in range(1,k):
         temp_price = temp_price - C*(1+spot_rate[i]/payment)**(-i)
+    # 計算即期利率
     spot_rate.append( (((1000+C)/temp_price)**(1/k)-1)*payment )
+    # 從第0期到第k期的遠期利率和即期利率相同
     forward_rate.append([spot_rate[k]])
+    # 計算遠期利率
     for i in range(1,k):
         forward_rate[k].append(   ( ( ((1+spot_rate[k]/payment)**k) \
                                /((1+forward_rate[i][0]/payment)**i) \
                                 )**(1/(k-i))-1 )*payment   )
 
+# 輸出結果
+#   如果輸出文字超過欄位，可以將輸出訊息複製到文字編輯軟體上，以利觀察輸出結果
 print("\n\n   計算結果")
 print("\n   到期時間  ",end="")
 print("             ",end="")
