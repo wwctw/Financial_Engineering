1970a0e15e0564857b893777dcf0d120750ec02d
wwctw <53468345+wwctw@users.noreply.github.com>
2020-05-12 14:05:29 +0800

    Add files via upload

--- HW5/hw5_r08323002.py
+++ HW5/hw5_r08323002.py
@@ -5,17 +5,20 @@ import numpy as np
 import QuantLib as ql
 
 # 輸入參數
-S0 = float(input("   請輸入現在股票價格 :  "))
-length = float(input("   請輸入到期時間(年) :  "))
-strike = float(input("   請輸入履約價格 :  "))
-sigma_stock = float(input("   請輸入股票年度化波動 sigma :  "))
-risk_free = float(input("   請輸入年化無風險利率(%) :  "))/100
+
 num_paths = int(input("   請輸入蒙地卡羅次數 :  "))
 timestep = int(input("   這期間總共要分幾步模擬 :  "))
 sigma = float(input("   請輸入Hull-White Model 的 sigma 值 :  "))
 a = float(input("   請輸入 Hull-White Model 的 a 值 :  "))
+negative = int(input("   允許負利率嗎  1 允許  2 不允許 :  "))
 forward_rate = float(input("   請輸入現在的年化利率(%) :  "))/100
-
+S0 = float(input("   請輸入現在股票價格 :  "))
+length = float(input("   請輸入到期時間(年) :  "))
+strike = float(input("   請輸入履約價格 :  "))
+sigma_stock = float(input("   請輸入股票年度化波動 sigma :  "))
+pv_method = int(input("   1 使用模擬出的利率折現  2 輸入無風險利率折現 :  "))
+if pv_method == 2:
+    risk_free = float(input("   請輸入年化無風險利率(%) :  "))/100
 
 # 用 Hull-White model 產生利率
 # 參考資料 :
@@ -41,10 +44,18 @@ seq = ql.GaussianPathGenerator(hw_process, length, timestep, rng, False)
 def generate_paths(num_paths, timestep):
     arr = np.zeros((num_paths, timestep+1))
     for i in range(num_paths):
-        sample_path = seq.next()
-        path = sample_path.value()
-        time = [path.time(j) for j in range(len(path))]
-        value = [path[j] for j in range(len(path))]
+        while 1:
+            sample_path = seq.next()
+            path = sample_path.value()
+            time = [path.time(j) for j in range(len(path))]
+            value = [path[j] for j in range(len(path))]
+            if negative == 1:
+                break
+            for k in range(0,len(value)):
+                if value[k]<0:
+                    break
+            if k == (len(value)-1):
+                break
         arr[i, :] = np.array(value)
     return np.array(time), arr
 
@@ -63,10 +74,10 @@ np.random.seed(200)
 def genBrownPath (T, mu_path, sigma_stock, S0, dt):
     n = len(mu_path)
     W = [0] + np.random.standard_normal(size = n)
-    W = np.cumsum(W)*np.sqrt(dt) # == standard brownian motion
+    W = np.cumsum(W)*np.sqrt(dt)
     X = (mu_path - 0.5*sigma_stock**2)*dt
     X = np.cumsum(X) + sigma_stock*W
-    S = S0*np.exp(X) # == geometric brownian motion
+    S = S0*np.exp(X)
     plt.plot(np.linspace(0, T, n), S)
     return S
 
@@ -77,14 +88,21 @@ put = []
 for i in range(0,num_paths):
     stock_paths.append(genBrownPath(length, paths[i], \
                                     sigma_stock, S0, length/timestep ) )
-    call.append( max(stock_paths[i][-1]-strike,0) )
-    put.append( max(strike-stock_paths[i][-1],0) )
+    if pv_method == 2:
+        dfactor = np.exp(risk_free*length)
+    else:
+        dfactor = 0
+        for j in range(0,len(paths[i])):
+            dfactor = dfactor + paths[i][j]
+        dfactor = np.exp(dfactor*length/timestep)
+    call.append( max(stock_paths[i][-1]-strike,0) / dfactor )
+    put.append( max(strike-stock_paths[i][-1],0) / dfactor )
 
 plt.title("Stock price simulation")
 plt.show()
 
-call_price = np.mean(call)/np.exp(-risk_free*length)
-put_price = np.mean(put)/np.exp(-risk_free*length)
+call_price = np.mean(call)
+put_price = np.mean(put)
 
 print("\n   買權價格為",'%9.3f'%call_price,"\n",end="")
 print("\n   賣權價值為",'%9.3f'%put_price,"\n",end="")
